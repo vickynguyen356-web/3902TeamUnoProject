3ce9745f8808cf94a4c799563ebd428f958b1b8c
vickynguyen356-web <vickynguyen356@gmail.com>
2026-09-24 19:14:37 -0400

    Updating Goomba enemy implementation

--- Entities/Goomba.cs
+++ Entities/Goomba.cs
@@ -14,6 +14,41 @@ namespace Sprint0.Entities
             Position = position;
         }
 
+        public Rectangle Bounds
+        {
+            get
+            {
+                return new Rectangle((int)Position.X, (int)Position.Y, Width, Height);
+            }
+        }
+
+        // Goomba doesn't have a small state.
+        public bool IsSmall
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        // Goomba doesn't have different forms.
+        public PlayerForm Form
+        {
+            get
+            {
+                return PlayerForm.Small;
+            }
+        }
+
+        public EntityAnimationState AnimationState
+        {
+            get
+            {
+                // TODO
+                return ;
+            }
+        }
+
         public void Update(GameTime gameTime)
         {
             // Enemy movement and state changes go here.
